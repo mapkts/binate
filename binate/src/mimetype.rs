ac79c891fd8296b56e789e737b39888306c3739a
Aaron Taner <mapkts@gmail.com>
2021-07-06 09:26:48 +0800

    feat: set default mimetype to binary

--- binate/src/mimetype.rs
+++ binate/src/mimetype.rs
@@ -1,7 +1,7 @@
 //! A list of wellknown MIME types.
 
 /// Default mimetype for encoding metadata and data.
-pub const DEFAULT_MIMETYPE: &str = "application/octet-stream";
+pub const DEFAULT_MIMETYPE: &str = "application/binary";
 
 /// Well-known MIME types.
 #[rustfmt::skip]
